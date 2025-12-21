16bb58755ddec88b61a5741913645e11497cd95e
benitojimenez <ebjpedro.2050@gmail.com>
2025-12-21 00:42:07 -0500

    actualiza las rutas del servidor para incluir modos de renderizado específicos

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,7 +3,52 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   
   {
-    path: '**',
+    path: '',  // Home
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'shop/**',  // Todas las rutas de shop (categorías, productos, etc.)
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'account/**',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'checkout/**',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'auth/**',
     renderMode: RenderMode.Prerender
+  },
+  // {
+  //   path: 'libro-reclamaciones',
+  //   renderMode: RenderMode.Client
+  // },
+  {
+    path: 'devolucion',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'privacidad',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'terminos-condiciones',
+    renderMode: RenderMode.Client
+  },
+  // {
+  //   path: 'comingsoon',
+  //   renderMode: RenderMode.Prerender
+  // },
+  {
+    path: '404',
+    renderMode: RenderMode.Prerender
+  },
+
+  {
+    path: '**',
+    renderMode: RenderMode.Server
   }
 ];
